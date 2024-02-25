7002446673f8d6692677aae3d5c29b8c63992539
RiverAge <744816609@qq.com>
2024-02-25 22:46:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import time
 from urllib import request
 from lxml import etree
 
+def toTB(string):
+  parts = string.split(' ')
+  if parts[1] == 'TB' or parts[1] == 'TiB':
+    return parts[0]
+  if parts[1] == 'GB' or parts[1] == 'GiB':
+    return str(float(parts[0]) / 1024)
+  if parts[1] == 'MB' or parts[1] == 'MiB':
+    return str(float(parts[0]) / 1024 / 1024)
+  if parts[1] == 'KB' or parts[1] == 'KiB':
+    return str(float(parts[0]) / 1024 / 1024 / 1024)
+  
 def u2(cookie):
   req = request.Request('https://u2.dmhy.org')
   req.add_header('cookie', cookie)
@@ -13,8 +24,8 @@ def u2(cookie):
           content = f.read().decode('utf-8')
           tree = etree.HTML(content)
           ratio = tree.xpath('//span[@class="color_ratio"]')[0].tail.strip();
-          upload = tree.xpath('//span[@class="color_uploaded"]')[0].tail.strip().split(' ')[0];
-          downloaded = tree.xpath('//span[@class="color_downloaded"]')[0].tail.strip().split(' ')[0];
+          upload = toTB(tree.xpath('//span[@class="color_uploaded"]')[0].tail.strip());
+          downloaded = toTB(tree.xpath('//span[@class="color_downloaded"]')[0].tail.strip());
           return [ratio, upload, downloaded]
 def ttg(cookie):
   req = request.Request('https://totheglory.im')
@@ -28,14 +39,30 @@ def ttg(cookie):
           tree = etree.HTML(content)
           span = tree.xpath('//td[@class="bottom"]/span[@class="smallfont"][1]/font')
           ratio = span[1].text
-          upload = span[3].xpath('./a')[0].text.split(' ')[0]
-          download = span[5].xpath('./a')[0].text.split(' ')[0]
+          upload = toTB(span[3].xpath('./a')[0].text)
+          download = toTB(span[5].xpath('./a')[0].text)
           return [ratio, upload, download]
+
+def pter(cookie):
+  req = request.Request('https://pterclub.com')
+  req.add_header('cookie', cookie)
+  req.add_header('user-agent', 'python urllib')
+  with request.urlopen(req) as f:
+        if f.status != 200:
+          return [0, 0, 0]
+        else:
+          content = f.read().decode('utf-8')
+          tree = etree.HTML(content)
+          ratio = tree.xpath('//font[@class="color_ratio"]')[0].tail.strip();
+          upload = toTB(tree.xpath('//font[@class="color_uploaded"]')[0].tail.strip());
+          downloaded = toTB(tree.xpath('//font[@class="color_downloaded"]')[0].tail.strip());
+          return [ratio, upload, downloaded]  
+
 def main(argv):
-  print("argv1", len(argv))
   r1 = u2(argv[1])
   r2 = ttg(argv[2])
-  r = time.strftime("%Y%m%d", time.localtime()) + " u:" + "|".join(r1) + " t:" + "|".join(r2)
+  r3 = pter(argv[3])
+  r = time.strftime("%Y%m%d", time.localtime()) + " u:" + "|".join(r1) + " t:" + "|".join(r2) + " p:" + "|".join(r3)
   with open('data.txt', 'a') as file:
      file.write(r + '\n');
 
