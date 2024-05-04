81c7fd8bc65e2325c742ab7a87680cc12e71b1d4
RiverAge <744816609@qq.com>
2024-05-04 14:16:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 import time
 from urllib import request
 from lxml import etree
+import json
+
 
 def toTB(string):
   parts = string.split(' ')
@@ -61,13 +63,41 @@ def pter(url, cookie):
           point = tree.xpath('//span[@class="medium"]/span[@class="color_bonus"]')[0].tail.strip()
           return [ratio, upload, downloaded, point]  
 
+def mt_updateLastBrowse(url, authorization):          
+  req = request.Request(url, method="POST")
+  req.add_header('authorization', authorization)
+  req.add_header('user-agent', 'python urllib')
+  req.add_header('content-type', 'multipart/form-data; boundary=----WebKitFormBoundary8EfqYPoNFf8YBSdt')
+  with request.urlopen(req) as f:
+    content = f.read().decode('utf-8')
+
+def mt(url, authorization):
+  mt_updateLastBrowse('/updateLastBrowse')
+  req = request.Request(url + '/profile', method="POST")
+  req.add_header('authorization', authorization)
+  req.add_header('user-agent', 'python urllib')
+  req.add_header('content-type', 'multipart/form-data; boundary=----WebKitFormBoundaryRI8m3nQthwqqPTO9')
+  with request.urlopen(req) as f:
+        if f.status != 200:
+          return [0, 0, 0]
+        else:
+          content = f.read().decode('utf-8')
+          resp = json.loads(content)
+          ratio = resp['data']['memberCount']['shareRate']
+          upload = toTB(str(int(resp['data']['memberCount']['uploaded']) / 1024) + ' KB')
+          download = toTB(str(int(resp['data']['memberCount']['downloaded']) / 1024) + ' KB')
+          point = resp['data']['memberCount']['bonus']
+          return [ratio, upload, download, point]
+
 def main(argv):
   r1 = u2(argv[1], argv[2])
   r2 = ttg(argv[3], argv[4])
   r3 = pter(argv[5], argv[6])
-  r = time.strftime("%Y%m%d", time.localtime()) + " u:" + "|".join(r1) + " t:" + "|".join(r2) + " p:" + "|".join(r3)
+  r4 = mt(argv[7], argv[8])
+  r = time.strftime("%Y%m%d", time.localtime()) + " u:" + "|".join(r1) + " t:" + "|".join(r2) + " p:" + "|".join(r3) + " m:" + "|".join(r4)
   with open('data.txt', 'a') as file:
-     file.write(r + '\n');
+     file.write(r + '\n')
+  
 
 if __name__ == '__main__':
   main(sys.argv)
